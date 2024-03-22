5143e89e49f94cb0957544601fc4e1f9367bfacc
Юлия <julia.m@list.ru>
2024-03-22 11:15:42 +0300

    My first commit

---
+++ yandex_api_stand_tests/configuration.py
@@ -0,0 +1,9 @@
+URL_SERVICE = "https://79dcfa08-341d-4d72-bb14-c04d85cacc27.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+# CREATE_USER_PATH хранит путь к API-методу для создания нового пользователя.
+# Этот путь будет использоваться для формирования полного URL-адреса в сочетании с базовым URL сервиса,
+# когда необходимо выполнить запрос на создание пользователя.
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
